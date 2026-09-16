ab7317e5dda2062a231e0ae91313753fd41369e5
tryout-gratis <minixsrv@gmail.com>
2026-09-16 12:44:41 +0700

    chore: update CSP to allow susercontent image sources
    Add `https://down-id.img.susercontent.com` and `https://*.susercontent.com` to the Content-Security-Policy `img-src` directive to allow loading images from Shopee/susercontent domains.

--- server.ts
+++ server.ts
@@ -166,7 +166,7 @@ app.use((req, res, next) => {
   res.setHeader('Permissions-Policy', 'camera=(), microphone=(), geolocation=()');
   res.setHeader(
     'Content-Security-Policy',
-    "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://challenges.cloudflare.com https://static.cloudflareinsights.com https://cusdis.com; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; img-src 'self' data: blob: https://res.cloudinary.com https://images.unsplash.com https://plus.unsplash.com https://ui-avatars.com; font-src 'self' data: https://fonts.gstatic.com; frame-src 'self' https://challenges.cloudflare.com https://cusdis.com https://www.youtube.com https://www.tiktok.com https://www.instagram.com; connect-src 'self' https://challenges.cloudflare.com https://cloudflareinsights.com https://static.cloudflareinsights.com https://cusdis.com https://api.cloudinary.com https://api.github.com;"
+    "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://challenges.cloudflare.com https://static.cloudflareinsights.com https://cusdis.com; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; img-src 'self' data: blob: https://res.cloudinary.com https://images.unsplash.com https://plus.unsplash.com https://ui-avatars.com https://down-id.img.susercontent.com https://*.susercontent.com; font-src 'self' data: https://fonts.gstatic.com; frame-src 'self' https://challenges.cloudflare.com https://cusdis.com https://www.youtube.com https://www.tiktok.com https://www.instagram.com; connect-src 'self' https://challenges.cloudflare.com https://cloudflareinsights.com https://static.cloudflareinsights.com https://cusdis.com https://api.cloudinary.com https://api.github.com;"
   );
   next();
 });
